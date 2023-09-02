493b15d51f8fcd772430092aa3362257fbe6b5ae
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-02 12:53:30 +0000

    [Fix] ルートファイルの記述順序を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-  resources :books, only: [:index, :show, :edit]
   resources :users, only: [:index, :show, :edit]
-  devise_for :users
+  resources :books, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
