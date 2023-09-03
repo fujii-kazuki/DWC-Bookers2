828cf5dd82346a7c471c8bdcae5b5b751404341c
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-03 17:48:04 +0000

    [Add] booksコントローラーのupdateルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   resources :users, only: [:index, :show, :edit]
-  resources :books, only: [:index, :show, :edit, :create, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
