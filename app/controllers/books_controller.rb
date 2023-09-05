7c281ac3a7640a05b91cc95bd3fe5d4f56d31215
fujii-kazuki <142955783+fujii-kazuki@users.noreply.github.com>
2023-09-05 08:32:36 +0000

    [Update] 他人の本の投稿編集ページにアクセスさせない

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @user = current_user
@@ -50,5 +51,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
+      redirect_to books_path
+    end
   end
 end
